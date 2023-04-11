96541df9181f05e9c08fb0ecf16d6a7cb8112778
unknown <patilvijaysing2092@gmail.com>
2023-04-11 23:31:42 +0530

    encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
+import com.sun.source.util.SourcePositions;
+
+import java.sql.SQLOutput;
+
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly reOnly = new RWOnly();
+
+        reOnly.setName("Vijay");
+        String ans = reOnly.getName();
+
+        System.out.println(ans);
+    }
 }
